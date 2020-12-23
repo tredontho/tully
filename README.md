7380eb59da06f48b5e61c790c06a2e5a6f695777
tredontho <tredontho@gmail.com>
2020-12-22 21:45:21 -0600

    Update README, Option type

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # tully
+
+### Features
+#### Done
+#### TODO
+##### Basic Balancesheet
+* Create account
+* Add/import transactions into account
+  * Transactions are movements of money between two accounts, or from/to a single account
+* Select all transactions within date range
+* Get statistics on account balance within date range
+  * Minimum/Maximum values, at least
+* Schedule transactions for future
+  * Some have a known action date, and can automatically resolve
+  * Some may have an approximate date, and should be manually resolved
+* Be able to manually adjust transaction details (date/amount)
+  * Keep audit/history of adjustments
+
+##### Trading
+* Import trade information
+* Store information like cost basis, trade date
+* Be able to handle complex trades (e.g. options, options strategies)
+* Be able to track/reslove options transactions which are related (e.g. closing out a position)
--- src/Option.hs
+++ src/Option.hs
@@ -1,5 +1,6 @@
 module Option
   ( OptionType (..),
+    OptionTxType (..),
     OptionStatus (..),
     Option,
     mkOption,
@@ -12,6 +13,9 @@ import Data.Time (Day)
 data OptionType = Call | Put
   deriving (Show, Eq)
 
+data OptionTxType = BuyToOpen | BuyToClose | SellToOpen | SellToClose
+  deriving (Show, Eq)
+
 data OptionStatus = Open | Closed | Assigned | Expired
   deriving (Show, Eq)
 
@@ -20,17 +24,21 @@ type Expiration = Day
 type StockSymbol = Text
 
 type Strike = Float
+
 type Price = Float
 
 data Option = Option
   { symbol :: StockSymbol,
     expiration :: Expiration,
+    openDate :: Day,
+    closeDate :: Maybe Day,
     optionType :: OptionType,
     strike :: Strike,
-    price :: Price,
+    openPrice :: Price,
+    closePrice :: Maybe Price,
     status :: OptionStatus
   }
   deriving (Show)
 
-mkOption :: StockSymbol -> Expiration -> OptionType -> Strike -> Price -> OptionStatus -> Option
-mkOption = Option
+mkOption :: StockSymbol -> Expiration -> Day -> OptionType -> Strike -> Price -> Option
+mkOption ss exp openDt ot strike openPrice = Option ss exp openDt Nothing ot strike openPrice Nothing Open
